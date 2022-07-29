5becc4a1efec170862d433ad5a71b945242aab6d
Anshaj Khare <anshaj13.ugcs20@iiitranchi.ac.in>
2022-07-29 22:24:22 +0530

    Added Button Functionalities

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.memeshare
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var memeImageView : ImageView
+    lateinit var progressBar : ProgressBar
+    lateinit var nextButton : Button
+    lateinit var shareButton: Button
+    var currentImageUrl : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        memeImageView = findViewById(R.id.memeImageView)
+        progressBar = findViewById(R.id.progressBar)
+        nextButton = findViewById(R.id.nextButton)
+        shareButton = findViewById(R.id.shareButton)
 
         loadMeme()
     }
@@ -22,29 +43,64 @@ class MainActivity : AppCompatActivity() {
     private fun loadMeme() {
 
         // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
-        val url = "https://www.google.com"
+
+        progressBar.visibility = View.VISIBLE
+        nextButton.isEnabled = false
+        shareButton.isEnabled = false
+        val url = "https://meme-api.herokuapp.com/gimme"
 
         // Request a string response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url,null,
             Response.Listener { response ->
-                val url = response.getString("url")
-                Glide.with(this).load(url).into(memeImageView)
+                currentImageUrl = response.getString("url")
+
+                Glide.with(this).load(currentImageUrl).listener(object: RequestListener<Drawable>{
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                }).into(memeImageView)
             },
             Response.ErrorListener {
                 Toast.makeText(this,"Something went wrong",Toast.LENGTH_SHORT).show()
             })
 
         // Add the request to the RequestQueue.
-        queue.add(jsonObjectRequest)
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
     fun shareMeme(view: View) {
 
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT,"Checkout this meme:-$currentImageUrl")
+        val chooser = Intent.createChooser(intent,"Share this meme using...")
+        startActivity(chooser)
+
     }
 
     fun nextMeme(view: View) {
+
+        loadMeme()
 
     }
 }
